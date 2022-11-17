682a279efd3cfb0fccccc79f57957d807bc99489
buftoronto <andy.weihuang@gmail.com>
2022-11-16 23:25:04 -0500

    Add project files.

---
+++ WebTestAzure1/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebTestAzure1.Models;
+using WebTestAzure1.Services;
+
+namespace WebTestAzure1.Pages
+{
+    public class IndexModel : PageModel
+    {
+      public List<Product> Products;  
+      public void OnGet()
+      {
+         var productService = new ProductService();
+         Products = productService.GetProducts();
+      }
+    }
+}
